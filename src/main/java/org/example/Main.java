9cf506fc8b52cbc85ed8b80202e32faa4e7206fc
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-19 11:16:28 -0300

    rm import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VirtualThreads virtualThreads = new VirtualThreads();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100); // empilha até 100 threads em paralelo
+        virtualThreads.executeTask(1000, executorService);
     }
 }
--- src/main/java/org/example/VirtualThreads.java
+++ src/main/java/org/example/VirtualThreads.java
@@ -1,2 +1,41 @@
-package org.example;public class VirtualThreads {
+package org.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VirtualThreads {
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public Runnable createTask() {
+        return () -> {
+            int taskId = counter.incrementAndGet();
+            // simula IO (entrada e saida), nao usa somente cpu
+            long timeout = (long) Math.random() * 1000 + 100;
+
+            System.out.println(String.format("Task com id %d executing for %s", taskId, timeout));
+
+            try {
+                Thread.sleep(timeout);
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+
+            System.out.println("Task done!");
+        };
+    }
+
+    public void executeTask(int quantidadeTarefas, ExecutorService executor) {
+        for (int i = 0; i < quantidadeTarefas; i++) {
+            executor.execute(createTask());
+        }
+
+        executor.shutdown(); // nao receber mais nenhum agendamento
+        try {
+            executor.awaitTermination(180, TimeUnit.MINUTES);
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 }
