e78f4619097ef78a54976a6f799d3d9897c1b796
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-19 11:36:54 -0300

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+A VirtualThread serve para otimizar processos que envolvem entrada e saída.
+
+Se fosse algo envolvendo apenas CPU, não haveria ganhos de desempenho. Ela não melhora desempenho de processamento. Apenas gerencia a utilização do recurso computacional, deixa a cargo da JVM.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,11 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) {
+        //defaultExecutor();
+        virtualThreadExecutor();
+    }
+
+    static void defaultExecutor() {
         VirtualThreads virtualThreads = new VirtualThreads();
 
         int nThreads = 100;
@@ -19,5 +24,20 @@ public class Main {
 
         long seconds = ChronoUnit.SECONDS.between(start, end);
         System.out.println(String.format("Done in %d seconds with %d threads", seconds, nThreads));
+    }
+
+    static void virtualThreadExecutor() {
+        VirtualThreads virtualThreads = new VirtualThreads();
+
+
+        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+        // nao se faz pool de threads, pois é um recurso barato. O sistema será gerenciado pela JVM
+
+        Instant start = Instant.now();
+        virtualThreads.executeTask(1000, executorService);
+        Instant end = Instant.now();
+
+        long seconds = ChronoUnit.SECONDS.between(start, end);
+        System.out.println(String.format("Done in %d seconds with VirtualThreads", seconds));
     }
 }
