5045d2a21c8ad9829c6a1ae81dc4b90bf054f179
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-19 11:18:49 -0300

    upd: cronometrando execucao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,6 +11,12 @@ public class Main {
         VirtualThreads virtualThreads = new VirtualThreads();
 
         ExecutorService executorService = Executors.newFixedThreadPool(100); // empilha até 100 threads em paralelo
+
+        Instant start = Instant.now();
         virtualThreads.executeTask(1000, executorService);
+        Instant end = Instant.now();
+
+        long seconds = ChronoUnit.SECONDS.between(start, end);
+        System.out.println(String.format("Done in %d seconds", seconds));
     }
 }
